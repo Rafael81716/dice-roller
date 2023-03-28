f83b467258929d88a883c69a78bdd3a7b6d8c1d7
Rafael Cavalcante <rafacavalcante081@gmail.com>
2023-03-28 16:31:35 -0300

    upgrades

--- ui.js
+++ ui.js
@@ -1,13 +1,4 @@
 // d4
-
-var dice = {
-    sides: 4,
-    roll: function () {
-      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-      return randomNumber;
-    }
-  }
-
   function printNumber(number) {
     var placeholder = document.getElementsByClassName('placeholder')[0];
     placeholder.innerHTML = number;
@@ -15,21 +6,12 @@ var dice = {
   
   var button = document.getElementsByClassName('button')[0];
   button.onclick = function() {
-    var result = dice.roll();
+    var result = dices.d4.roll();
     printNumber(result);
   };
 
 
 //d6
-
-  var dice1 = {
-    sides: 6,
-    roll: function () {
-      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-      return randomNumber;
-    }
-  }
-
   function printNumber1(number) {
     var placeholder = document.getElementsByClassName('placeholder')[1];
     placeholder.innerHTML = number;
@@ -37,20 +19,12 @@ var dice = {
   
   var button = document.getElementsByClassName('button')[1];
   button.onclick = function() {
-    var result = dice1.roll();
+    var result = dices.d6.roll();
     printNumber1(result);
   };
 
 
 //d8
-var dice2 = {
-  sides: 8,
-  roll: function () {
-    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    return randomNumber;
-  }
-}
-
 function printNumber2(number) {
   var placeholder = document.getElementsByClassName('placeholder')[2];
   placeholder.innerHTML = number;
@@ -58,18 +32,11 @@ function printNumber2(number) {
 
 var button = document.getElementsByClassName('button')[2];
 button.onclick = function() {
-  var result = dice2.roll();
+  var result = dices.d8.roll();
   printNumber2(result);
 };
 
 //d10
-var dice3 = {
-  sides: 10,
-  roll: function () {
-    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    return randomNumber;
-  }
-}
 
 function printNumber3(number) {
   var placeholder = document.getElementsByClassName('placeholder')[3];
@@ -78,18 +45,11 @@ function printNumber3(number) {
 
 var button = document.getElementsByClassName('button')[3];
 button.onclick = function() {
-  var result = dice3.roll();
+  var result = dices.d10.roll();
   printNumber3(result);
 };
 
 //d12
-var dice4 = {
-  sides: 12,
-  roll: function () {
-    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    return randomNumber;
-  }
-}
 
 function printNumber4(number) {
   var placeholder = document.getElementsByClassName('placeholder')[4];
@@ -98,19 +58,11 @@ function printNumber4(number) {
 
 var button = document.getElementsByClassName('button')[4];
 button.onclick = function() {
-  var result = dice4.roll();
+  var result = dices.d12.roll();
   printNumber4(result);
 };
 
 //d12
-var dice5 = {
-  sides: 100,
-  roll: function () {
-    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    return randomNumber;
-  }
-}
-
 function printNumber5(number) {
   var placeholder = document.getElementsByClassName('placeholder')[5];
   placeholder.innerHTML = number;
@@ -118,6 +70,51 @@ function printNumber5(number) {
 
 var button = document.getElementsByClassName('button')[5];
 button.onclick = function() {
-  var result = dice5.roll();
+  var result = dices.d100.roll();
   printNumber5(result);
 };
+
+const dices = {
+  d4: {
+    sides: 4,
+    roll: function () {
+      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+      return randomNumber;
+    }
+  },
+  d6: {
+    sides: 6,
+    roll: function () {
+      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+      return randomNumber;
+    }
+  },
+  d8: {
+    sides: 8,
+    roll: function () {
+      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+      return randomNumber;
+    }
+  },
+  d10: {
+    sides: 10,
+    roll: function () {
+      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+      return randomNumber;
+    }
+  },
+  d12: {
+    sides: 12,
+    roll: function () {
+      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+      return randomNumber;
+    }
+  },
+  d100: {
+    sides: 100,
+    roll: function () {
+      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+      return randomNumber;
+    }
+  },
+}
